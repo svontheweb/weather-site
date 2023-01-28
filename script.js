b0b209c53c35e25f9bbced7b8d998e2c88321df8
V <svontheweb@gmail.com>
2023-01-29 03:27:06 +0530

    display func to extract attributes of weather obj in response

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ let weather = {
         +'&units=metric&appid='
         +this.apiKey)
     .then((response) => response.json())
-.then((data) => console.log(data));
-}
+.then((data) => this.displayWeather(data));
+} ,
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed }  = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+    }
 };
