9f28424fd1abb8a297298d6588b9c0dea4ca0df2
V <svontheweb@gmail.com>
2023-01-29 18:55:08 +0530

    same

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ let weather = {
         document.querySelector('.humidity').innerText = "Humidity: " + humidity + "%";
         document.querySelector('.wind').innerText =  "Wind Speed: " + speed + " km/h";
         document.querySelector('.weather').classList.remove('loading');
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1920x1080/?" + name + "')";
 
     },
     search: function() {
